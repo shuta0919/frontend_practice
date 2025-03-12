19863359d8dbd82acd292e3ff9f36d0344fac3a8
shuta0919 <murahiro0919@gmail.com>
2025-03-12 10:41:31 +0900

    fix: 不要な記述を削除

--- src/app/_components/SearchForm/index.tsx
+++ src/app/_components/SearchForm/index.tsx
@@ -2,7 +2,7 @@
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import 'jest-styled-components';
+
 
 const SearchContainer = styled.div`
   display: flex;
