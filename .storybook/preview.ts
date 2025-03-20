b4e576d4c857388cb6b7955d88db3be79187f924
shuta0919 <murahiro0919@gmail.com>
2025-03-20 21:48:08 +0900

    add: storybookの設定を追加

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,9 @@
 import type { Preview } from "@storybook/react";
+import '../src/app/globals.css';
 
 const preview: Preview = {
   parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
